b551c94ab8bd03adea1b6e4dcbafebc34038f25c
Cameron Lay <cameronlay15@gmail.com>
2020-10-08 15:30:55 -0400

    initial commit

---
+++ db/local/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES 
+("Squirrel", 0),
+("Fish", 0),
+("Gary", 0),
+("Veggie", 1),
+("Beef", 1),
+("Chicken", 1),
+("Bison", 1),
+("Ostrich", 1);
